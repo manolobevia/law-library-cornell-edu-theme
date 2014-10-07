c6f22af0fb97434b23b832ae4483097a6f3b1ca9
Manolo Bevia <mb863@cornell.edu>
2014-10-07 12:56:08 -0400

    Adding images for homepage

---
+++ js/utils.js
@@ -0,0 +1,61 @@
+
+$(document).ready(function () {
+
+    $('.rs-carousel').carousel({
+        pagination: false,
+        itemsPerTransition:3,
+        insertPrevAction: function () {
+        return $('<a href="#" class="rs-carousel-action-prev"><</a>').appendTo($(this).find('.carousel-prev'));
+        },
+        insertNextAction: function () {
+        return $('<a href="#" class="rs-carousel-action-next">></a>').appendTo($(this).find('.carousel-next'));
+        }
+        
+    });
+
+    // Make carousel responsive
+    $(window).resize(function () {
+        $('.rs-carousel').carousel('refresh');
+    });
+
+
+});
+
+/*
+
+REFERENCE
+$('.rs-carousel').carousel({
+    itemsPerTransition:1,
+    pagination:false,
+    insertPrevAction: function () {
+      return $('<a href="#" class="rs-carousel-action-prev"></a>').appendTo($(this).closest('.carousel-container').find('.carousel-prev'));
+    },
+    insertNextAction: function () {
+      return $('<a href="#" class="rs-carousel-action-next"></a>').appendTo($(this).closest('.carousel-container').find('.carousel-next'));
+    }
+  }).show(400, function() {
+    var $rsCarousel = $(this);
+
+    // Show the paging controls if they are needed
+    if ($rsCarousel.carousel('getNoOfPages') > 1) {   
+      $rsCarousel.closest('.carousel-container').find('.paging-link').show();
+    }
+  });
+  
+  $('.carousel-prompt').click(function() {
+    $(this).next('.proposal-topics').fadeOut('slow').fadeIn('slow');
+  });
+
+  // Make carousel responsive
+  $(window).resize(function () {
+    $('.rs-carousel').carousel('refresh');
+  });
+
+
+
+
+  insertPrevAction: function () { return $('<a href="#" class="rs-carousel-action rs-carousel-action-prev"><</a>').appendTo(this); },
+        insertNextAction: function () { return $('<a href="#" class="rs-carousel-action rs-carousel-action-next">></a>').appendTo(this); }
+
+
+*/
