602c801bdae8c21555ddd0dad2e25c6860c235d6
Manolo Bevia <mb863@cornell.edu>
2014-10-08 15:14:07 -0400

    style header pages

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -99,24 +99,6 @@
 </header>
 
 
-<nav class="navbar">
-  <div class="container">
-    <div class="row">
-      <div class="span4 logo">
-        <?php if ($site_name): ?>
-         <h1><a href="<?php print $front_page; ?>"><span class="displace"><?php print $site_name; ?>. Meanderings through Aby Warburg's Atlas</span></a></h1>
-         <!--<a class="toggle-logo" title="phonetic" href="#"></a>-->
-        <?php endif; ?>
-      </div>
-      <div class="span8">
-        <div class="nav">
-          <?php print render($page['navigation']); ?>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
-
 <!-- HEADER IMAGE -->
 <section class="">
     <img src="/sites/all/themes/law_library_bootstrap/images/header-interior.jpg" />
@@ -130,9 +112,10 @@
         <strong>Our Space</strong>
     </nav>  
 
-    <h2 class="title-content"><?php print $title; ?></h2>
-
     <div class="main-content col-xs-12 col-sm-12 col-md-9 col-lg-9">
+
+        <h2 class="title-content"><span><?php print $title; ?></span></h2>
+
 		<?php print render($page['content']); ?>
 	</div>
 
