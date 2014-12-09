8f2f210f491a9144e95fb63a1a3870015beac0d3
starsplatter <jenncd@gmail.com>
2014-12-09 12:20:32 -0500

    breadcrumb separator again

--- template.php
+++ template.php
@@ -7,4 +7,7 @@
 
 function law_library_bootstrap_breadcrumb($variables) {
   $sep = ' &gt; ';
+  if (count($variables['breadcrumb']) > 0) {
+    return implode($sep, $variables['breadcrumb']) . $sep;
+  }
 }
