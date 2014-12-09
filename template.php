2f8532316a6199b9d4b5d598587c1683396c31d8
starsplatter <jenncd@gmail.com>
2014-12-09 12:26:35 -0500

    remove breadcrumb function

--- template.php
+++ template.php
@@ -5,9 +5,3 @@
  * template.php
  */
 
-function law_library_bootstrap_breadcrumb($variables) {
-  $sep = ' &gt; ';
-  if (count($variables['breadcrumb']) > 0) {
-    return implode($sep, $variables['breadcrumb']) . $sep;
-  }
-}
