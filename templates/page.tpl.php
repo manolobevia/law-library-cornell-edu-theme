5f74a70034ca91d27638805f4c1058ad507c5244
Manolo Bevia <mb863@cornell.edu>
2014-12-18 15:01:14 -0500

    search interior

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -84,8 +84,35 @@ $theme_path = base_path() . 'sites/all/themes/law_library_bootstrap/';
 
     <div class="search-interior">
 
+        <form id="form1" name="form1" method="get" action="http://newcatalog.library.cornell.edu">
+            <label for="name">Search</label>
+            <div class="filtered-search">
+
+                <input type="text" class="form-text" name="q" value="" /><select name="search_type" id="search_type" size="1" onChange="chgAction()" class="form-control filter">
+
+                    <option value="law" selected="selected">Law Collections <span> ></span></option>
+                    <option value="catalog">Library Catalog</option>
+                    <option value="worldcat">WorldCat</option>
+
+                </select>
+
+                <div class="hide-select-btn"></div>
+
+                <input type="hidden" name="f[lc_1letter_facet][]" id ="callno" value ="K - Law">
+                <input type="hidden" name="search_field" value="all_fields">
 
 
+                <!--a class="filter" href="#">LAW LIBRARY <span class="caret-icon">></span></a> -->
+                <button type="submit" class="submit-search" href="#"><span class="glyphicon glyphicon-search"></span></button>
+
+            </div>
+        </form>         
+    
+        <ul class="external-search">
+            <li><a href="#">My Library Account</a> / </li>
+            <li><a href="#">Databases</a> / </li>
+            <li><a href="#">Research Guides</a></li>
+        </ul>
 
     </div>
 
