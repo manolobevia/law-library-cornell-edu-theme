47ce5f3f1fe73ff1f83695e832cfcf7959356cd3
Manolo Bevia <mb863@cornell.edu>
2014-12-18 14:02:41 -0500

    search interior

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -80,14 +80,7 @@ $theme_path = base_path() . 'sites/all/themes/law_library_bootstrap/';
 
 
 
-<section class="search-slider">
 
-    <div class="search-interior">
-
-
-    </div>
-
-</section>
 
 
 <!-- CUL BRANDING  <?php print $theme_path; ?> -->
@@ -102,6 +95,14 @@ $theme_path = base_path() . 'sites/all/themes/law_library_bootstrap/';
                 <li><a title="Today's Hours" href="#"><span class="glyphicon glyphicon-time" aria-hidden="true"></span></a></li>
                 <li><a title="Room Reservations"href="#"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></a></li>
             </ul>
+            <section class="search-slider">
+
+    <div class="search-interior">
+
+
+    </div>
+
+</section>
         </nav>
 
             
