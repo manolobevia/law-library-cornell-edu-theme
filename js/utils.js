ecb0692d4b249074dfafdc4c15210036749e07fc
Manolo Bevia <mb863@cornell.edu>
2015-01-15 11:51:14 -0500

    Added new  responsive search to home page

--- js/utils.js
+++ js/utils.js
@@ -94,21 +94,152 @@ $(document).ready(function () {
                 
             });
 
+    /////////////////////////////
+    // Home search filter
+    // Toggle filter select list
+
+
+     var $searchFilterList = $('#filter-search-nav');
+     var $isFilterOpen = false;
+
+     $('div.filter-search').click(function(e) {
+         e.preventDefault();
+
+         if (!$isFilterOpen) {
+
+             //Change button filter state to selected
+             
+             $(this).find('span').removeClass('filter-default');
+             $(this).find('span').addClass('filter-active');
+
+             //Reveal filter select list
+             $searchFilterList.css('display','block');
+
+             $isFilterOpen = true;
+
+         } else {
+             //Change button filter state to default
+             
+             $(this).find('span').removeClass('filter-active');
+             $(this).find('span').addClass('filter-default');
+
+             //Hide filter select list
+             $searchFilterList.css('display','none');
+
+             $isFilterOpen = false;
+
+         }
+    });
+
+     //
+     //
+     // Collect users' selection
+     //
+     // 
+        var $selectedFilter = "Library Catalog";
+
+       $('#filter-search-nav li').each(function(index){
+           $(this).click(function(ev){
+              
+              ev.preventDefault();
+
+              //console.log($(this).text());
+              //Selected filter feedback
+              $('.search-filter-selected').text($(this).text()).fadeIn('slow');
+              $('input[name="classgroup"]').val('');
+
+              $('.filter-search .active').text($(this).text());
+
+              //$selectedFilter = $(this).text();
+
+              //console.log($(this).parent());
+
+               //Hide filter select list
+               //$searchFilterList.css('display','none');
+
+               //console.log($searchFilterList);
+
+               //Change button filter state to default
+               //$('a.filter-search').css('background','url(../../themes/vivo-cornell/images/filteredSearch.gif) no-repeat right top');
+               $('a.filter-search').removeClass('filter-active');
+               $('a.filter-search').addClass('filter-default');
+
+               
+               $isFilterOpen = false;
+
+
+           });
+
+       });
+
+
+        
+
+       //When focus, hide filter select list and change filter button state to default
+       $('input.search-homepage').focus(function(){
+
+           $('input.search-homepage').attr("value","");
+           $('input.search-homepage').css({
+               'text-align' : 'left',
+               'opacity' : 1
+           });
+
+           if (!$isFilterOpen) {
+
+               $isFilterOpen = false;
+
+           }else {
+
+                //Hide filter select list
+                    $('#filter-search-nav').hide();
+
+                    //Change button filter state to default
+                    //$('a.filter-search').css('background','url(../../themes/vivo-cornell/images/filteredSearch.gif) no-repeat right top');
+                    $('div.filter-search').find('span').removeClass('filter-active');
+                    $('div.filter-search').find('span').addClass('filter-default');
+
+                    $isFilterOpen = false;
+
+             }
+
+       });
+
+       
+
+
+       $( '#search' ).submit(function(ev) {
+          
+          var send = $('.search-homepage').val() + '    ' + $selectedFilter;
+          console.log(send);
+
+          switch ($selectedFilter) {
+
+                case 'Library Catalog':
+                    $(this).attr("action", 'https://search.library.cornell.edu');
+                    break;
+
+                case 'WorldCat':
+                    $(this).attr("action", 'http://cornell.worldcat.org/search?qt=wc_org_cornell');
+                    break;
+
+                case 'Activities':
+
+                    $(this).attr("method", 'get');
+                    $('#edit_keys').name = "keys";
+                    $(this).attr("action", '/search/node/');
+                    break;
+            }
+
+
+
+          //$(this).attr("action", 'https://search.library.cornell.edu');
+
+        });
+
+    
+
 
 });
 
 
- 
 
-
-    /*<script>
-
-    http://jockstothecore.com/proper-implementation-of-background-images-with-text-inside-responsive-carousels/
-      !function ($) {
-        $(function(){
-          // carousel demo
-          $('#myCarousel').carousel()
-        })
-      }(window.jQuery)
-    </script>
-    */
--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -104,32 +104,50 @@ $theme_path = base_path() . 'sites/all/themes/law_library_bootstrap/';
 <!-- SEARCH BOX -->       
 <section class="search-home">
 
-    <form id="form1" name="form1" method="get" action="https://search.library.cornell.edu">
-        <label for="name">Search</label>
+    <!-- Pay attention to METHOD -->
+    <form id="search" name="form1" method="get" action="https://search.library.cornell.edu">
+
+        <label for="name">Search <span class="search-filter-selected">Library Catalog</span></label>
+
+
         <div class="filtered-search">
 
-            <input type="text" class="form-text" name="q" value="" id="edit_keys" /><select name="search_type" id="search_type" size="1" onChange="chgAction()" class="form-control filter">
+            <input type="text" class="search-homepage" name="q" value="" id="edit_keys" />
 
-                <option value="catalog" selected="selected">Library Catalog ></option>
+            <!--<select name="search_type" id="search_type" size="1" onChange="chgAction()" class="form-control filter">
+
+                <option value="catalog" selected="selected">Library Catalog</option>
                 <option value="worldcat">WorldCat</option>
                 <option value="site">Site Search</option>
 
-            </select>
+            </select>-->
 
-            <div class="hide-select-btn"></div>
+            <div class="filter-search"><span class="active">Library Catalog</span> <span class="glyphicon glyphicon-chevron-down filter-default"></span>
+
+              <ul id="filter-search-nav">
+                  <li role="listitem"><a href="#" title="">Library Catalog</a></li>
+                  <li role="listitem"><a href="#" title="">WorldCat</a></li>
+                  <li role="listitem"><a href="#" title="">Activities</a></li>
+              </ul>
+
+          </div>
+
+          
 
             <!--a class="filter" href="#">LAW LIBRARY <span class="caret-icon">></span></a> -->
-            <button type="submit" class="submit-search" href="#"><span class="glyphicon glyphicon-search"></span></button>
+            <button type="submit" class="submit-search" href="#">
+              <span class="glyphicon glyphicon-search"></span>
+            </button>
 
         </div>
+
+        <ul class="external-search">
+          <li><a href="https://www.library.cornell.edu/myacct">My Library Account</a> / </li>
+          <li><a href="http://guides.library.cornell.edu/onlinelegalresources">Databases</a> / </li>
+          <li><a href="http://guides.library.cornell.edu/LAW">Research Guides</a></li>
+        </ul>
     </form>         
     
-    <ul class="external-search">
-        <li><a href="https://www.library.cornell.edu/myacct">My Library Account</a> / </li>
-        <li><a href="http://guides.library.cornell.edu/onlinelegalresources">Databases</a> / </li>
-        <li><a href="http://guides.library.cornell.edu/LAW">Research Guides</a></li>
-    </ul>
-
 </section>
 
 <!-- RESEARVATION HOURS --> 
