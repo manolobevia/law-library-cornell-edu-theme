fa387c6ed18e1d61d58a7f3ceb19b0577639a4cc
starsplatter <jenncd@gmail.com>
2015-01-23 15:00:29 -0500

    improve active selection

--- js/utils.js
+++ js/utils.js
@@ -233,7 +233,7 @@ $(document).ready(function () {
   var url = window.location.hash.split('/')[0];
 
 $('nav li a').filter(function() {
-    return this.href == url;
+    return this.href.hash.split('/')[0] == url;
 }).addClass('active');
 
 
